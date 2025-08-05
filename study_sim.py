2707928bedb682356157c6acc2d2357754fdfba8
Manuel <manuel.stocchi3@gmail.com>
2025-08-05 22:33:27 +0200

    Add new files

--- study_sim.py
+++ study_sim.py
@@ -12,7 +12,7 @@ data = np.load("t011.npy")
 data2 = np.load("tdie.npy")
 data3 = np.load("t012.npy")
 data4 = np.load("u012.npy")
-data4m = np.load("t014.npy")
+data4m = np.load("t015.npy")
 
 
 # median = np.median(np.log(data[:,-1]))
@@ -34,8 +34,8 @@ ax1.plot([0,1000],[1e6, 1e6], label ="Nisyros", color="green", linestyle="--")
 ax1.set_ylabel("Releasable energy [MJ]")
 # ax1.set_yscale("log")
 ax2 = ax1.twinx()
-ax2.set_ylabel("Tons of TNT")
-ax2.plot([0,1000], [1800,1800], color="orange", linestyle="--", label = "Beirut 2020 ")
+# ax2.set_ylabel("Tons of TNT")
+# ax2.plot([0,1000], [1800,1800], color="orange", linestyle="--", label = "Beirut 2020 ")
 
 # ax1.plot(data[:,2], data[:,-1]*1e-6, color=[0.8,0.6, 0])
 # ax2.plot(data[:,2], data[:,-1]/(4.184*1e9), color=[0.8,0.6, 0])
@@ -46,7 +46,9 @@ ax2.plot([0,1000], [1800,1800], color="orange", linestyle="--", label = "Beirut 
 (data4m[:,-1]>1e16)
 
 ax1.plot(data4m[:,2], data4m[:,-1]*1e-6, color=[0.1,0.1, 0.7])
-ax2.plot(data4m[:,2], data4m[:,-1]/(4.184*1e9), color=[0.1,0.1, 0.7])
+# ax2.plot(data4m[:,2], data4m[:,-1]/(4.184*1e9), color=[0.1,0.1, 0.7])
+
+ax2.plot(data4m[:,2], data4m[:,-2], color='red')
 
 
 # ax1.plot(data[:,2], data[:,-1]*1e-6, color=[0.8,0.3, 0], label="100 mW/m2")
@@ -64,10 +66,21 @@ fag.legend()
 
 feg, ex = plt.subplots(dpi=300)
 ex.set_title("Overpressure ratio")
-ex.plot(data2[:,2], data2[:,-3], color=[0.4,0.3, 0.4], label="0 mW/m2")
-ex.plot(data[:,2], data[:,-3], color=[0.8,0.3, 0], label="100 mW/m2")
+# ex.plot(data2[:,2], data2[:,-3], color=[0.4,0.3, 0.4], label="0 mW/m2")
+# ex.plot(data[:,2], data[:,-3], color=[0.8,0.3, 0], label="100 mW/m2")
 ex.plot(data4m[:,2], data4m[:,-3], color=[0.,0.3, 0.8], label="500 mW/m2")
 # ex.set_xlabel(r"T$_{\text{Bot}}$ [°C]")
-
+ex2 = ex.twinx()
+ex2.plot(data4m[:,2], data4m[:,-2], color='red')
 feg.legend()
 
+
+
+fog, ox = plt.subplots(dpi=300)
+ox.set_title("Sim time")
+# ox.plot(data2[:,2], data2[:,-2], color=[0.4,0.3, 0.4], label="0 mW/m2")
+# ox.plot(data[:,2], data[:,-2], color=[0.8,0.3, 0], label="100 mW/m2")
+ox.plot(data4m[:,2], data4m[:,-2], color=[0.,0.3, 0.8], label="500 mW/m2")
+# ex.set_xlabel(r"T$_{\text{Bot}}$ [°C]")
+
+fog.legend()
