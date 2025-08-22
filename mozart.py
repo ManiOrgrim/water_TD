1036f1ff7ce1c73509e78dd37189d20020ab5b6c
Manuel <manuel.stocchi3@gmail.com>
2025-08-22 18:46:27 +0200

    Added ensemble mode

--- mozart.py
+++ mozart.py
@@ -1,22 +1,22 @@
 import os
 import subprocess
-from infilewriter import modify_input
+from infilewriter import modify_input_sources
 import pexpect
 from BOOMER import boom_eval
 import numpy as np
 
-def singlerun(name, number, flux, Tbot, HPperm, HPporo, LPperm, LPporo, dZ):
-    runname = name+"{:04d}".format(number)
+def singlerun(name, number, flux, Tbot, HPperm, HPporo, LPperm, LPporo, inmass, intemp,  dZ):
+    runname = name+"{:02d}".format(number)
     if (runname not in os.listdir()):
              os.mkdir(runname)
     os.chdir(runname)
-    with open(runname+'.in', 'w') as vraito:
-        texto = modify_input(flux, Tbot, HPperm, HPporo, LPperm, LPporo, dZ)
-        vraito.write(texto)
-    os.system('echo "{}" > comandi'.format(runname+'.in'))
-    os.system('echo "{}" >> comandi'.format(runname+'.out'))
-    os.system('cat "comandi" | ../ht.exe')
-    print("Calculating explosivity..., run number ", number )
+    # with open(runname+'.in', 'w') as vraito:
+    #     texto = modify_input_sources(flux, Tbot, HPperm, HPporo, LPperm, LPporo, inmass, intemp,  dZ)
+    #     vraito.write(texto)
+    # os.system('echo "{}" > comandi'.format(runname+'.in'))
+    # os.system('echo "{}" >> comandi'.format(runname+'.out'))
+    # os.system('cat "comandi" | ../ht.exe')
+    print("Calculating explosivity..., run number ", name )
     overcome, runtime, explosivity = boom_eval(runname)
     #print(explosivity)
     return overcome, runtime, -explosivity[0]
@@ -53,28 +53,80 @@ runs = []
 
 
 
-simnames = ["D110B","D125B", "D150B", "D175B", "D200B", "D225B", "C250B"]
-Tbots    = [110, 125, 150, 175, 200, 225, 250]
+simnames = ["G110C","G125C", "G150C", "G175C", "G200C", "G225C", "G250C", "G300C", "G500C", "G650C", "G800C", "G999C"]
+Tbots    = [110, 125, 150, 175, 200, 225, 250, 300, 500,650, 800, 999]
 
-HPperm = 2e-12
-HPporo = 0.154
+# simnames = ["H150E"]
+# Tbots    = [150]
+
+def from_millidarcy2sqr_cm(millidarcy):
+    return millidarcy*9.863e-12
+
+HPperm = from_millidarcy2sqr_cm(346)
+HPporo = 0.372
 LPperm = 1.0e-28
 LPporo = 1.0e-02#0.01+np.random.rand()*0.09
-Dz =  10
+Dz =  50
 
-for jcond in range (len(simnames)):
-    flux = 0 #`tipycal ranges around 20000
+# for jcond in range (len(simnames)):
+#     flux = 0 #`tipycal ranges around 20000
+#     runs = []
+#     simname = simnames[jcond]
+#     Tbot = Tbots[jcond]
+#     for irun in range(41):
+#         flux = irun*10000
+#         OCmean, endtime, explo = singlerun(simname, irun, flux,Tbot,HPperm,HPporo,LPperm,LPporo,Dz)
+#         runs.append(np.array([irun, flux,Tbot,HPperm,HPporo,LPperm,LPporo,Dz, OCmean, endtime, explo]))
+#         os.chdir("..")
+  
+#     print(runs)
+#     runs = np.array(runs)
+#     np.save(simname+".npy", runs)
+
+
+simnames = ["F010_100","F010_200","F010_300","F050_100","F050_200","F050_300","F100_100","F100_200","F100_300"]    
+inmasses = [10,10,10,50,50,50,100,100,100]
+intempes = [100,200,300,100,200,300,100,200,300]
+
+
+# for jcond in range (len(simnames)):
+#     flux = 0 #`tipycal ranges around 20000
+#     runs = []
+#     simname = simnames[jcond]
+#     Tbot = 150
+#     for irun in range(41):
+#         flux = irun *10000
+#         inmass = inmasses[jcond]
+#         intemp = intempes[jcond]
+#         OCmean, endtime, explo = singlerun(simname, irun, flux,Tbot,HPperm,HPporo,LPperm,LPporo,inmass, intemp, Dz)
+#         runs.append(np.array([irun, flux,Tbot,HPperm,HPporo,LPperm,LPporo,Dz, OCmean, endtime, explo]))
+#         os.chdir("..")
+  
+#     print(runs)
+#     runs = np.array(runs)
+#     np.save(simname+".npy", runs),    mass flux 10**[-3, 3]
+seed = 2    #HC: seed 0, multisource,   mass flux 10**[-3, 3]
+            #IC: seed 1, monosource     mass flux 10**[-3, 3]
+            #JC: seed 2, Ttop 50°C      mass flux 10**[-3, 3]
+            #KC: seed 3, Ttop 50°C      mass flux 10**[-5, 1]
+np.random.seed(seed)
+simchevogliovedere = [73]
+for jcond in range (900):
+    flux = 10000*np.random.rand()*4
+    Tbot = np.random.rand()*250+50
+    intemp = np.random.rand()*250+50
+    inmass = 10**(np.random.rand()*6-5)
     runs = []
-    simname = simnames[jcond]
-    Tbot = Tbots[jcond]
-    for irun in range(41):
-        flux = irun*10000
-        OCmean, endtime, explo = singlerun(simname, irun, flux,Tbot,HPperm,HPporo,LPperm,LPporo,Dz)
-        runs.append(np.array([irun, flux,Tbot,HPperm,HPporo,LPperm,LPporo,Dz, OCmean, endtime, explo]))
+    simname = "JC{:03d}_".format(jcond)
+    for irun in range(1):
+        OCmean, endtime, explo = singlerun(simname, irun, flux,Tbot,HPperm,HPporo,LPperm,LPporo,inmass, intemp, Dz)
+        runs.append(np.array([irun, flux,Tbot,intemp, inmass,HPperm,HPporo,LPperm,LPporo, Dz, OCmean, endtime, explo]))
         os.chdir("..")
   
     print(runs)
     runs = np.array(runs)
     np.save(simname+".npy", runs)
+    
+
 
 #singlerun("mozzd", irun, flux,Tbots,1e-10,0.1,1e-15,0.02,2)
